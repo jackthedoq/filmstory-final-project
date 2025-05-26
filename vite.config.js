2a878c3bbba2bda77e6cee539b7ccda974e792ff
jackthedoq <aajr301103@gmail.com>
2025-05-27 03:31:17 +0700

    Fix conflict and restore deleted files

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
-export default {
-    server: {
-      host: '0.0.0.0', // Listen on all interfaces
-      port: 8000,      // Optional: Change if needed
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/filmstory-final-project/',
+  publicDir: 'public',
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    host: '0.0.0.0', // Listen on all interfaces
+      port: 8000       // Optional: Change if needed
     }
-  }
+  });
